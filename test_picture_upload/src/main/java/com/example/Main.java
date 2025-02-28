4341307e229174d0bc7507b543d10c69690f1175
Joe <joseph.winfield@legrand.com>
2025-02-28 16:30:06 -0600

    start work on GET request for all images with defined substring in name

--- test_picture_upload/src/main/java/com/example/Main.java
+++ test_picture_upload/src/main/java/com/example/Main.java
@@ -3,6 +3,9 @@ package com.example;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,24 +15,16 @@ import org.apache.http.util.EntityUtils;
 
 public class Main {
 
-    static String url = "http://10.0.0.19:8080/sprites/id/452";
+    static String single_url = "http://10.0.0.19:8080/sprites/id/452";
+    static String multiple_url = "http://10.0.0.19:8080/sprites/contains/test";
 
     public static void main(String[] args) throws IOException{
-        SpriteData sprite = new SpriteData();
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            setSpriteData(httpClient, sprite);
-        }
-
-        try (FileOutputStream fileOutputStream = new FileOutputStream(new File("test.jpg"))) {
-            fileOutputStream.write(sprite.data);
-            System.out.println("Sprite saved to " + "test.jpg");
-        }
-
+        // getSingleImage();
+        getMultipleImages();
     }
 
     private static void setSpriteData(CloseableHttpClient httpClient, SpriteData sprite) throws IOException{
-        HttpGet request = new HttpGet(url);
+        HttpGet request = new HttpGet(single_url);
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getStatusLine().getStatusCode() == 200) {
                 byte[] responseBody = EntityUtils.toByteArray(response.getEntity());
@@ -40,4 +35,50 @@ public class Main {
             }
         }
     }
+
+    private static void getSingleImage() throws IOException {
+        SpriteData sprite = new SpriteData();
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            setSpriteData(httpClient, sprite);
+        }
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(new File("test.jpg"))) {
+            fileOutputStream.write(sprite.data);
+            System.out.println("Sprite saved to " + "test.jpg");
+        }
+    }
+
+    private static void getMultipleImages() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            saveImageDirectory(setMultipleSpritesData(httpClient));
+        }
+    }
+
+    private static List<SpriteData> setMultipleSpritesData(CloseableHttpClient httpclient) throws IOException{
+        HttpGet request = new HttpGet(multiple_url);
+        List<SpriteData> sprites = new ArrayList<SpriteData>();
+        try(CloseableHttpResponse response = httpclient.execute(request)){
+            if (response.getStatusLine().getStatusCode() == 200){
+                String body = EntityUtils.toString(response.getEntity());
+                for(String line : body.split("\n")){
+                    byte[] image = Base64.getDecoder().decode(line);
+                    sprites.add(new SpriteData(image));
+                }
+            } else {
+                System.err.println("Failed to fetch sprite collection at: " + response.getStatusLine());
+            }
+        }
+        return sprites;
+    }
+
+    private static void saveImageDirectory(List<SpriteData> sprites) throws IOException {
+        int image_num = 1;
+        for(SpriteData sprite : sprites){
+            try (FileOutputStream fileOutputStream = new FileOutputStream(new File("test.jpg"))) {
+                fileOutputStream.write(sprite.data);
+                System.out.println("Sprite saved to " + "test-" + String.valueOf(image_num) + ".jpg");
+            }
+        }
+    } 
 }
--- test_picture_upload/src/main/java/com/example/SpriteData.java
+++ test_picture_upload/src/main/java/com/example/SpriteData.java
@@ -3,6 +3,12 @@ package com.example;
 public class SpriteData {
     public byte[] data;
 
+    SpriteData(){}
+
+    SpriteData(byte[] data){
+        this.data = data;
+    }
+
     public void setSpriteData(byte[] data){
         this.data = data;
     }
